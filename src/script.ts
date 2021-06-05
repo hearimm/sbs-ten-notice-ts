3c2e83cfa64e45ac10ec8dc0ece7bf77b828ce77
Hulk Choi <doksul23@gmail.com>
2021-06-06 05:05:09 +0900

    moment timezone added

--- src/script.ts
+++ src/script.ts
@@ -2,9 +2,12 @@ import { noticeService } from './services/notice/noticeService'
 import { scheduleTextReaderService } from './services/scheduleTextReader/scheduleTextReaderService';
 import { scheduleTargetSend } from './services/schedule/scheduleTargetSender';
 import { sendTelegramQueue } from './services/telegram/sendTelegramQueue';
+import * as moment from 'moment-timezone'
 
 const config = require('dotenv').config()
 const runAll = async () => {
+    moment.tz.setDefault("Asia/Seoul");
+    console.log(moment().format('YYYYMMDD_HHmmss'))
     console.log('----------------------start--------------------')
     await noticeService();
     await scheduleTextReaderService();
